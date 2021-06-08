a37b6e3ed17b3de6bc0a76601a6b808d20e1ea16
Quan Li <quan.li@cvitek.com>
2021-06-09 00:19:20 +0800

    update dirs

---
+++ 算法/leetcode/数组/977_sortedSquares.cpp
@@ -0,0 +1,57 @@
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> sortedSquares(std::vector<int>& nums) {
+        std::vector<int> ret;
+        if (nums[0] >= 0) {
+            for (auto val : nums) {
+                ret.push_back(val*val);
+            }
+            return ret;
+        }
+
+        if (nums[nums.size()-1] < 0) {
+            for (auto it = nums.rbegin(); it != nums.rend(); ++it) {
+                ret.push_back(*it * *it);
+            }
+            return ret;
+        }
+        
+        int div = 0;
+        for (; div < nums.size(); ++div) {
+            if (nums[div] >= 0) {
+                break;
+            }
+        }
+
+        // 0 ~ div-1  div ~ len-1
+        int left = div-1;
+        int right = div;
+
+        while (left >= 0 || right <= nums.size()-1) {
+            if (nums[left] * nums[left] <= nums[right] * nums[right]) {
+                ret.push_back(nums[left] * nums[left]);
+                left--;
+            } else {
+                ret.push_back(nums[right] * nums[right]);
+                right++;
+            }
+
+            if (left < 0) {
+                while (right < nums.size()) {
+                    ret.push_back(nums[right] * nums[right]);
+                    right++;
+                }
+            }
+
+            if (right > nums.size()-1) {
+                while (left >= 0) {
+                    ret.push_back(nums[left] * nums[left]);
+                    left--;
+                }
+            }
+        }
+        return ret;
+    }
+};
