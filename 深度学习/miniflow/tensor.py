2f15ea8ea2a8cacdbf62172ea6fc8ef03cedaf03
Quan Li <quan.li@cvitek.com>
2021-07-06 22:36:24 +0800

    update

--- 深度学习/miniflow/tensor.py
+++ 深度学习/miniflow/tensor.py
@@ -6,13 +6,26 @@ class Tensor(object):
       self._data = data
     else:
       raise "data is not an instance of numpy ndarray"
-  
+
+    self._creator = None
+
   @property
-  def shape():
+  def data(self):
+    return self._data
+
+  @property
+  def shape(self):
     return self._data.shape
+
+  @setattr
+  def data(self, data):
+    if isinstance(data, np.ndarray):
+      self._data = data
+    else:
+      raise "data is not an instance of numpy ndarray"
 
   def reshape(self, shape):
     self._data = np.reshape(self._data)
 
   def device(self):
-    return "cpu" if isinstance(self._data, np.ndarray) else "gpu"
+    return "cpu"
